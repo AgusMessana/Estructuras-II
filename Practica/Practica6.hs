d5db0d39a2f7457fe489f8ee2d56184c164bedef
AgusMessana <agustin200369@gmail.com>
2026-05-29 17:07:48 -0300

    Ejercicios práctica 6

--- Practica/Practica6.hs
+++ Practica/Practica6.hs
@@ -93,3 +93,86 @@ S(h) = S(h - 1) + O(1) ∈ O(h)
 -- =============================================================================
 -- Ejercicio 2
 -- =============================================================================
+data Tree a = E | Leaf a | Join (Tree a) (Tree a)
+
+-- ### Inciso a)
+mcss :: (Num a, Ord a) => Tree a -> a
+mcss arbol = m
+  where
+    (m, p, s, tot) = mapReduce tuplaHoja combinarTuplas (0, 0, 0, 0) arbol
+
+combinarTuplas :: (Num a, Ord a) => (a, a, a, a) -> (a, a, a, a) -> (a, a, a, a)
+combinarTuplas (ml, pl, sl, tl) (mr, pr, sr, tr) = (mcssNuevo, prefijoNuevo, sufijoNuevo, totalNuevo)
+  where
+    totalNuevo = tl + tr
+    prefijoNuevo = max pl (tl + pr)
+    sufijoNuevo = max sr (tr + sl)
+    mcssNuevo = max ml (max mr (sl + pr))
+
+tuplaHoja :: (Num a, Ord a) => a -> (a, a, a, a)
+tuplaHoja v = (max v 0, max v 0, max v 0, v)
+
+mapReduce :: (a -> b) -> (b -> b -> b) -> b -> Tree a -> b
+mapReduce f c base E = base
+mapReduce f c base (Leaf v) = f v
+mapReduce f c base (Join izq der) = c izq' der'
+  where
+    (izq', der') = mapReduce f c base izq ||| mapReduce f c base der
+
+-- ### Inciso b)
+{-
+W(n) = W(n_izq) + W(n_der) + O(1) ∈ O(n)
+S(n) = máx{S(h_izq), S(h_der)} + O(1) ∈ O(h)
+-}
+
+-- =============================================================================
+-- # Ejercicio 3
+-- =============================================================================
+mejorGanancia :: Tree Int -> Int
+mejorGanancia arbol = maxAll arbolDeGanancias
+  where
+    pares = conSufijos arbol
+    arbolDeGanancias = mapTree' calcularGanancias pares
+
+calcularGanancias :: (Int, Tree Int) -> Tree Int
+calcularGanancias (compra, arbolVentas) = mapTree' (\venta -> venta - compra) arbolVentas
+
+mapTree' :: (a -> b) -> Tree a -> Tree b
+mapTree' f E = E
+mapTree' f (Leaf v) = Leaf (f v)
+mapTree' f (Join izq der) =
+  let (izq', der') = mapTree' f izq ||| mapTree' f der
+   in Join izq' der'
+
+sufijos :: Tree Int -> Tree (Tree Int)
+sufijos E = E
+sufijos (Leaf _) = Leaf E
+sufijos (Join izq der) = Join nuevoIzq (sufijos der)
+  where
+    nuevoIzq = mapTree' (\t -> Join t der) (sufijos izq)
+
+juntar :: Tree Int -> Tree (Tree Int) -> Tree (Int, Tree Int)
+juntar E E = E
+juntar (Leaf v) (Leaf s) = Leaf (v, s)
+juntar (Join izq1 der1) (Join izq2 der2) = Join (juntar izq1 izq2) (juntar der1 der2)
+
+conSufijos :: Tree Int -> Tree (Int, Tree Int)
+conSufijos E = E
+conSufijos (Leaf v) = Leaf (v, E)
+
+maxT :: Tree Int -> Int
+maxT = reduce max minBound
+
+reduce :: (a -> a -> a) -> a -> Tree a -> a
+reduce c base E = base
+reduce c base (Leaf v) = v
+reduce c base (Join izq der) = c izq' der'
+  where
+    (izq', der') = reduce c base izq ||| reduce c base der
+
+maxAll :: Tree (Tree Int) -> Int
+maxAll = mapReduce maxT max minBound
+
+-- =============================================================================
+-- # Ejercicio 4
+-- =============================================================================
